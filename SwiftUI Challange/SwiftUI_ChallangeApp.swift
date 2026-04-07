423c85ac0d64c25664dd783460e85e870593d7b1
Johnson <elangbamj@avaya.com>
2026-04-07 05:41:42 +0530

    Initial Commit

---
+++ SwiftUI Challange/SwiftUI_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChallangeApp.swift
+//  SwiftUI Challange
+//
+//  Created by Johnson on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
